e633e3f42bf1cc726e84ce83698faa065ca96a15
Nithya03sk <43105336+Nithya03sk@users.noreply.github.com>
2019-04-10 22:59:49 +0530

    Update addtwo.c

--- addtwo.c
+++ addtwo.c
@@ -1,15 +1,36 @@
+//pass by value
 #include<stdio.h>
 int add(int a,int b)
 {
-return a+b;
+  return a+b;
 }
 void main()
 {
-int a,b;
-printf("enter a");
-scanf("%d",&a);
-printf("enter b");
-scanf("%d",&b);
-add(a,b);
-printf("%d+%d is %d",a,b,a+b);
+  int a,b,sum=0;
+  printf("enter a");
+  scanf("%d",&a);
+  printf("enter b");
+  scanf("%d",&b);
+  sum=add(a,b);
+  printf("sum is %d",sum);
 }
+
+//pass by reference
+#include<stdio.h>
+#include<conio.h>
+int add(int *a,int *b)
+{
+       return *a+*b;
+}
+void main()
+{
+	int a,b,sum=0;
+	clrscr();
+	printf("enter a and b\n");
+	scanf("%d%d",&a,&b);
+	sum=add(&a,&b);
+	printf("sum is %d",sum);
+	getch();
+}
+
+
