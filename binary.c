d53f86b6dfabca4ed28e586b9ac5c00e46b82f58
Nithya03sk <43105336+Nithya03sk@users.noreply.github.com>
2019-09-30 04:56:59 -0700

    Create binary.c

---
+++ binary.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void input(int n,int a[n])
+{
+    int a[100],n;
+    printf("enter array elements\n");
+    for(i=0;i<n;i++)
+    {
+         scanf("%d",&a[i]);
+    }
+}
+int compute(int n,int a[n])
+{
+    int pivot=p[0];
+    for(i=0;i<n;i++)
+    
+}
